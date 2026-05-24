0205bc6113ce79d2a3d939b7823342518eeab33a
SetarehPishiyar <pishiyarsetareh@gmail.com>
2026-05-24 23:42:55 +0330

    Init

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import ButtonGradient from "./assets/svg/ButtonGradient";
+import Button from "./components/Button";
+import Header from "./components/Header";
+
+const App = () => {
+  return (
+    <>
+      <Header />
+    </>
+  );
+};
+
+export default App;
